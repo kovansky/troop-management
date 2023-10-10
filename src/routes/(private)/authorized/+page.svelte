2a874d7c30520aeb3a7a140d0c718cb5fe9c9694
RMaster121 <rs.szczerba@hotmail.com>
2023-10-10 23:53:43 +0200

    Added password reset option

--- src/routes/(private)/authorized/+page.svelte
+++ src/routes/(private)/authorized/+page.svelte
@@ -7,16 +7,19 @@
 	import { toast } from 'svelte-french-toast';
 	import type { PageData } from './$types';
 
-    let selectedPerson = null;
+	let selectedPerson = null;
 
 	export let data: PageData;
 
-    const addAccess = async (event) => {
-        const form = new FormData(event.target);
-        if (!selectedPerson || !selectedPerson?.id) {
-            toast.error('Nie wybrano osoby!');
-            return;
-        }
+	let { supabase, session } = data;
+	$: ({ supabase, session } = data);
+
+	const addAccess = async (event) => {
+		const form = new FormData(event.target);
+		if (!selectedPerson || !selectedPerson?.id) {
+			toast.error('Nie wybrano osoby!');
+			return;
+		}
 		form.append('id', selectedPerson.id);
 		const res = await fetch(`/api/auth/add_access`, {
 			method: 'POST',
@@ -32,26 +35,34 @@
 			toast.error('Wystąpił błąd! - ' + body);
 			console.log('Error:', status, body);
 		}
-    }
+	};
+	const resetPassword = async () => {
+		toast.loading('Wysyłanie emaila z linkiem do resetu hasła...');
+		const { data, error } = await supabase.auth.resetPasswordForEmail(session.user.email, {
+			redirectTo: window.location.origin + '/invited'
+		});
+		toast.dismiss();
+		if (error) {
+			console.log('Error:', error);
+			toast.error('Wystąpił błąd! - ' + error.message);
+		} else {
+			toast.success('Wysłano email z linkiem do resetu hasła!');
+		}
+	};
+
+	function closeDialog() {
+		(document.getElementById('person_modal') as HTMLFormElement).close();
+		selectedPerson = null;
+	}
 </script>
 
 <main class="h-full pb-16 overflow-y-auto">
 	<div class="container grid px-6 mx-auto">
-		<div class="flex justify-between items-center">
-			<div>
-				<h2 class="mt-6 mb-4 text-2xl font-semibold text-gray-700 dark:text-gray-200">
-					Użytkownicy
-				</h2>
-				<h4 class="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
-					Lista osób z dostępem do jednostki
-				</h4>
-			</div>
-			<button
-				class="px-4 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-green-600 rounded-md hover:bg-green-500 focus:outline-none focus:bg-green-500"
-				on:click={() => goto(`${$page.url}/person_details`)}
-			>
-				Dodaj harcerza
-			</button>
+		<div>
+			<h2 class="mt-6 mb-4 text-2xl font-semibold text-gray-700 dark:text-gray-200">Użytkownicy</h2>
+			<h4 class="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+				Lista osób z dostępem do jednostki
+			</h4>
 		</div>
 		<div class="w-full mb-8 overflow-hidden rounded-lg shadow-xs">
 			<div class="w-full overflow-x-auto">
@@ -64,7 +75,7 @@
 							<th class="px-4 py-3">Funkcja</th>
 							<th class="px-4 py-3">Zastęp główny</th>
 							<th class="px-4 py-3">Stopień</th>
-                            <th class="px-4 py-3">Dostęp</th>
+							<th class="px-4 py-3">Dostęp</th>
 							<th class="px-4 py-3">Akcja</th>
 						</tr>
 					</thead>
@@ -72,7 +83,10 @@
 						{#each data.people as person}
 							<tr
 								class="text-gray-700 dark:text-gray-400 hover:bg-gray-100 cursor-pointer"
-                                on:click={()=>{ document.getElementById('person_modal').showModal(); selectedPerson = person;}}
+								on:click={() => {
+									document.getElementById('person_modal').showModal();
+									selectedPerson = person;
+								}}
 							>
 								<td class="px-4 py-3">
 									<div class="flex items-center text-sm">
@@ -99,7 +113,10 @@
 									color={person.degrees?.color}
 									title={person.degrees?.name ?? 'HBS'}
 								/>
-                                <ColorTag color={person.fk_user_id ? 'rgba(0, 128, 0, 0.75)' : 'grey'} title={person.fk_user_id ? 'Przyznano' : 'Nie przyznano'} />
+								<ColorTag
+									color={person.fk_user_id ? 'rgba(0, 128, 0, 0.75)' : 'grey'}
+									title={person.fk_user_id ? 'Przyznano' : 'Nie przyznano'}
+								/>
 								<td class="px-4 py-3">
 									<button
 										class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
@@ -121,13 +138,15 @@
 	</div>
 </main>
 <dialog id="person_modal" class="modal">
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <!-- svelte-ignore a11y-no-static-element-interactions -->
-    <div class="border rounded-lg shadow relative max-w-2xl modal-box" on:click|stopPropagation>
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<div class="modal-backdrop" on:click|stopPropagation={closeDialog} />
+	<div class="border rounded-lg shadow relative max-w-2xl modal-box">
 		<div class="flex justify-end p-2">
 			<button
 				type="button"
 				class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
+				on:click={closeDialog}
 			>
 				<svg
 					class="w-5 h-5"
@@ -158,34 +177,69 @@
 					d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
 				/>
 			</svg>
-            <h2 class="mb-2 text-2xl font-semibold text-gray-700 dark:text-gray-200">Uprawnienia dostępu</h2>
-          <p class="text-xl mb-3 font-semibold text-gray-700 dark:text-gray-200">Czy chcesz przyznać dostęp do dokumentacji jednostki użytkownikowi <b>{capitalizeEveryWord(selectedPerson?.name || '')}</b>?</p>
-            <form id="email-form" method="dialog" on:submit={addAccess}>
-            <label for="email">Adres email użytkownika</label>
-										<input
-											type="email"
-											name="email"
-											id="email"
-											class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-											placeholder="jan.kowalski@example.com"
-                                            required
-										/>
-            </form>
-			<div class="flex justify-center mt-5">
-               
-				<button
-                    form="email-form"
-					class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-base inline-flex items-center px-3 py-2 text-center mr-2"
-				>
-					Tak, przyznaj
-				</button>
-                <form method="dialog" on:submit={() => selectedPerson = null}><button
-					class="text-gray-900 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-cyan-200 border border-gray-200 font-medium inline-flex items-center rounded-lg text-base px-3 py-2 text-center"
-				>
-					Nie, anuluj
-				</button></form>
-				
-			</div>
+			<h2 class="mb-2 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+				Uprawnienia dostępu
+			</h2>
+			{#if !selectedPerson?.fk_user_id}
+				<p class="text-xl mb-3 font-semibold text-gray-700 dark:text-gray-200">
+					Czy chcesz przyznać dostęp do dokumentacji jednostki użytkownikowi <b
+						>{capitalizeEveryWord(selectedPerson?.name || '')}</b
+					>?
+				</p>
+				<form id="email-form" method="dialog" on:submit={addAccess}>
+					<label for="email">Adres email użytkownika</label>
+					<input
+						type="email"
+						name="email"
+						id="email"
+						class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+						placeholder="jan.kowalski@example.com"
+						required
+					/>
+				</form>
+				<div class="flex justify-center mt-5">
+					<button
+						form="email-form"
+						class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-base inline-flex items-center px-3 py-2 text-center mr-2"
+					>
+						Tak, przyznaj
+					</button>
+					<form method="dialog" on:submit={closeDialog}>
+						<button
+							class="text-gray-900 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-cyan-200 border border-gray-200 font-medium inline-flex items-center rounded-lg text-base px-3 py-2 text-center"
+						>
+							Nie, anuluj
+						</button>
+					</form>
+				</div>
+			{/if}
+			{#if selectedPerson?.fk_user_id}
+				<p class="text-xl mb-3 font-semibold text-gray-700 dark:text-gray-200">
+					Użytkownik <b>{capitalizeEveryWord(selectedPerson?.name || '')}</b> ma już dostęp do dokumentacji
+					jednostki.
+				</p>
+				<!-- decide if delete or reset password or cancel -->
+				<div class="flex justify-center mt-5">
+					<button
+						class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-base inline-flex items-center px-3 py-2 text-center mr-2"
+					>
+						Zabierz dostęp
+					</button>
+					<button
+						class="text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-base inline-flex items-center px-3 py-2 text-center mr-2"
+						on:click={resetPassword}
+					>
+						Zresetuj hasło
+					</button>
+					<form method="dialog" on:submit={closeDialog}>
+						<button
+							class="text-gray-900 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-cyan-200 border border-gray-200 font-medium inline-flex items-center rounded-lg text-base px-3 py-2 text-center"
+						>
+							Anuluj
+						</button>
+					</form>
+				</div>
+			{/if}
 		</div>
 	</div>
-  </dialog>
+</dialog>
