40301537efeea10490d659dadcc788fef48e8c32
RMaster121 <rs.szczerba@hotmail.com>
2023-10-13 00:59:05 +0200

    Auto route from dashboard to people

--- src/routes/(private)/dashboard/+page.svelte
+++ src/routes/(private)/dashboard/+page.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
     import type { PageData } from './$types';
+	import { goto } from '$app/navigation';
     
     export let data: PageData;
+
+    onMount(() => {
+        goto('/people');
+    });
 </script>
