d2260c829b68145faacb0d2b9c165afdbf334710
RMaster121 <rs.szczerba@hotmail.com>
2023-10-23 22:47:37 +0200

    Added UI for editng transactions

--- src/routes/(private)/finance/finance_details/+page.svelte
+++ src/routes/(private)/finance/finance_details/+page.svelte
@@ -8,7 +8,7 @@
 	import { onMount } from 'svelte';
 	export let data: PageData;
 
-	let returnPath = '/people';
+	let returnPath = '/finance';
 
 	onMount(() => {
 		const urlParams = new URLSearchParams(window.location.search);
@@ -69,35 +69,86 @@
 					<div class="lg:col-span-2">
 						<form on:submit|preventDefault={savePerson}>
 							<div class="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
-								<div class="md:col-span-4">
+								<div class="md:col-span-5">
 									<label for="name">Nazwa</label>
 									<input
 										type="text"
 										name="name"
 										id="name"
 										class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-										value={data.finance.name}
+										value={data.finance?.name || ''}
 										placeholder="Chusty harcerskie"
 										minlength="3"
 										required
 									/>
 								</div>
-                                <!-- kwota -->
-                                <div class="md:col-span-1">
-
-								<div class="md:col-span-5">
-									<label for="address">Ulica i numer domu</label>
+								<div class="md:col-span-2">
+									<label for="amount">Kwota</label>
+									<div class="relative">
+										<div
+											class="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none"
+										>
+											PLN
+										</div>
+										<input
+											type="text"
+											id="amount"
+											class="block pl-11 h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+											value="{data.finance?.amount || ''}"
+											placeholder="21,37"
+										/>
+									</div>
+								</div>
+								<div class="md:col-span-1">
+									<label for="type">Rodzaj transakcji</label>
+									<select
+										name="type"
+										id="type"
+										class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+									>
+										<option value="expense" selected={data.finance.amount < 0}>Wydatek</option>
+										<option value="income" selected={data.finance.amount > 0}>Przychód</option>
+									</select>
+								</div>
+								<div class="md:col-span-2">
+									<label for="category">Kategoria</label>
+									<select
+										name="category"
+										id="category"
+										class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+									>
+										<option value="" selected={data.finance.fk_finance_category_id === null}>Brak</option>
+										{#each data.categories as category}
+											<option value={category.id} selected={data.finance.fk_finance_category_id === category.id}>	
+												{category.name}
+											</option>										
+										{/each}
+									</select>
+								</div>
+								<div class="md:col-span-3">
+									<label for="doc_number">Numer dokumentu</label>
 									<input
 										type="text"
-										name="address"
-										id="address"
+										name="doc_number"
+										id="doc_number"
 										class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-										value={data.finance.description}
-										placeholder="Główna 1"
+										value={data.finance?.description || ''}
+										placeholder="FV 123/2021"
+									/>
+								</div>
+								<!-- Date -->
+								<div class="md:col-span-2">
+									<label for="date">Data</label>
+									<input
+										type="date"
+										name="date"
+										id="date"
+										class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+										value={data.finance?.date || ''}
+										required
 									/>
 								</div>
 
-								
 								<!-- Row -->
 								<!-- <div class="md:col-span-2">
 									<label for="group">Zastęp</label>
@@ -114,7 +165,7 @@
 										<option value="" selected={!person?.fk_small_group_id}> Brak zastępu </option>
 									</select>
 								</div> -->
-							
+
 								<!-- row -->
 								<div class="inline-flex float-right pt-4">
 									<div class="px-2">
