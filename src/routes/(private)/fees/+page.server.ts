ddba99272a67644ae580907eb6dcaecd8b0ed290
RMaster121 <rs.szczerba@hotmail.com>
2023-11-01 19:39:05 +0100

    Changed order

--- src/routes/(private)/fees/+page.server.ts
+++ src/routes/(private)/fees/+page.server.ts
@@ -2,7 +2,7 @@
 export async function load({ locals: { supabase } }) {
   const { data: fees_types, error: db_error1 } = await supabase
     .from("fees_types").select(`
-  id, name, amount, small_groups (name), count_finance, start_date, is_formal`);
+  id, name, amount, small_groups (name), count_finance, start_date, is_formal`).order("start_date", { ascending: false });
   if (db_error1) throw db_error1;
   return { fees_types };
 }
