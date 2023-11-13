ebc4ae9747be26101fd415095b1c281eaaed9bd0
RMaster121 <rs.szczerba@hotmail.com>
2023-11-13 12:56:07 +0100

    UI updated

--- src/routes/(private)/people/person_details/+page.svelte
+++ src/routes/(private)/people/person_details/+page.svelte
@@ -6,7 +6,6 @@
 	import toast from 'svelte-french-toast';
 	import type { PageData } from './$types';
 	import { onMount } from 'svelte';
-	import { circIn } from 'svelte/easing';
 	export let data: PageData;
 
 	let returnPath = '/people';
@@ -28,8 +27,6 @@
 		const { status, body } = res;
 		if (status === 200 || status === 201) {
 			toast.success('Zapisano!');
-			// invalidateAll();
-			// window.location.replace('/people');
 			goto(returnPath, { invalidateAll: true });
 		} else if (status === 400) {
 			toast.error('Błędne dane! - ' + body);
@@ -71,7 +68,6 @@
 		if (status === 204) {
 			toast.success('Usunięto!');
 			invalidateAll();
-			closeDialog();
 		} else {
 			toast.error('Wystąpił błąd! - ' + res);
 			console.log('Error:', status, body);
@@ -111,6 +107,14 @@
 	function closeDialog() {
 		(document.getElementById('file_modal') as HTMLFormElement).close();
 	}
+
+	const loadFile = function(event) {
+    	var output = document.getElementById('form-avatar') as HTMLFormElement;
+    	output.src = URL.createObjectURL(event.target.files[0]);
+    	output.onload = function() {
+      		URL.revokeObjectURL(output.src)
+    	}
+  	};
 </script>
 
 <main class="h-screen p-6 bg-gray-100 flex items-center justify-center">
@@ -126,10 +130,9 @@
 							<p>Wypełnij pola obowiązkowe</p>
 						</div>
 
-						<!-- It may fail -->
 						<div class="flex w-3/4 mt-4 relative z-0">
 							<div
-								class="absolute z-10 hover:bg-neutral-focus items-center flex-col gap-y-2 flex w-3/4 h-full opacity-0 hover:opacity-100 justify-center"
+								class="absolute rounded z-10 hover:bg-neutral-focus items-center flex-col gap-y-2 flex w-3/4 h-full opacity-0 hover:opacity-100 justify-center"
 							>
 								<button
 									class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex"
@@ -151,7 +154,7 @@
 											d="M12 10.5v6m3-3H9m4.06-7.19l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"
 										/>
 									</svg>
-									Upload
+									Prześlij
 								</button>
 								<button
 									class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded flex"
@@ -171,12 +174,16 @@
 											d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
 										/>
 									</svg>
-									Delete
+									Usuń
 								</button>
 							</div>
-							<div class="avatar placeholder w-3/4 z-0">
+							<div class="avatar {data.streamed.picture ? '' : 'placeholder'} w-3/4 z-0">
 								<div class="bg-neutral-content text-neutral-focus rounded w-full">
-									<span class="text-3xl">{getFirstLetters(data.streamed.person.name || '')}</span>
+									{#if data.streamed.picture && data.streamed.picture !== ''}
+										<img class="text-3xl" src={data.streamed.picture} alt='' />
+									{:else}
+										<span class="text-3xl">{getFirstLetters(data.streamed.person.name || '')}</span>
+									{/if}
 								</div>
 							</div>
 						</div>
@@ -480,8 +487,10 @@
 				Zdjęcie profilowe harcerza
 			</h2>
 			<div class="mt-4">
-				<form on:submit|preventDefault={uploadAvatar} id="avatar-form">
-					<input name="file" type="file" class="file-input file-input-bordered w-full max-w-xs" />
+				<form on:submit|preventDefault={uploadAvatar} id="avatar-form" class="flex flex-col items-center">
+					<!-- if image is in form show it -->
+					<input name="file" type="file" class="file-input file-input-bordered w-full max-w-xs" on:change={loadFile} />
+					<img id="form-avatar" class="mt-4 w-1/2" alt="" />
 					<div class="md:col-span-5">
 						<button
 							form="avatar-form"
