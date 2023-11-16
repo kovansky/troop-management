2a51500efa1884a333290545059cc587613374f0
RMaster121 <rs.szczerba@hotmail.com>
2023-11-16 12:24:08 +0100

    Update fee edit page styling for dark mode

--- src/routes/(private)/fees/fee_edit/+page.svelte
+++ src/routes/(private)/fees/fee_edit/+page.svelte
@@ -57,14 +57,14 @@
 	};
 </script>
 
-<div class="h-screen p-6 bg-gray-100 flex items-center justify-center">
+<main class="h-screen p-6 bg-gray-100 flex items-center justify-center dark:bg-gray-900">
 	<div class="container max-w-screen-lg mx-auto">
 		<div>
-			<h2 class="font-semibold text-xl text-gray-600 pb-4">Edytuj składkę</h2>
+			<h2 class="font-semibold text-xl text-gray-600 pb-4 dark:text-gray-300">Edytuj składkę</h2>
 
-			<div class="bg-white rounded shadow-lg p-4 px-4 md:p-8 mb-6">
+			<div class="bg-white rounded shadow-lg p-4 px-4 md:p-8 mb-6 dark:bg-gray-800">
 				<div class="grid gap-4 gap-y-2 text-sm grid-cols-1 lg:grid-cols-3">
-					<div class="text-gray-600">
+					<div class="text-gray-600 dark:text-gray-400">
 						<p class="font-medium text-lg">Szczegóły składki</p>
 						<p>Wypełnij pola obowiązkowe</p>
 					</div>
@@ -110,7 +110,6 @@
 									<select
 										name="small_group"
 										id="small_group"
-										class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
 									>
 										<option value="" selected={data.fee_type.fk_small_group_id === null}
 											>Cała jednostka</option
@@ -198,4 +197,4 @@
 			</div>
 		</div>
 	</div>
-</div>
+</main>
