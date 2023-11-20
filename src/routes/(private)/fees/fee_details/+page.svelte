8708c0396178ed8150958a714e052aea276bba7a
RMaster121 <rs.szczerba@hotmail.com>
2023-11-20 22:04:01 +0100

    Add PersonAvatarText component to authorized and fee_details pages

--- src/routes/(private)/fees/fee_details/+page.svelte
+++ src/routes/(private)/fees/fee_details/+page.svelte
@@ -10,6 +10,7 @@
 	import { onMount } from 'svelte';
 	import MainPage from '$lib/components/MainPage.svelte';
 	import TableView from '$lib/components/TableView.svelte';
+	import PersonAvatarText from '$lib/components/PersonAvatarText.svelte';
 	export let data: PageData;
 
 	let showModal = false;
@@ -67,39 +68,7 @@
 					showModal = true;
 				}}
 			>
-				<td>
-					<div class="flex items-center text-sm">
-						<div class="relative hidden w-8 h-8 mr-3 rounded-full md:block">
-							{#await data.streamed.picturesList}
-								<img
-									class="object-cover w-full h-full rounded-full"
-									src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
-									alt=""
-									loading="lazy"
-								/>
-							{:then picturesList}
-								<img
-									class="object-cover w-full h-full rounded-full"
-									src={picturesList.find(
-										(picture) => person.id.toString() == picture.name.split('.')[0].toString()
-									)?.url?.signedUrl ||
-										'https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'}
-									alt=""
-									loading="lazy"
-								/>
-							{/await}
-							<div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true" />
-						</div>
-						<div>
-							<p class="font-semibold">
-								{capitalizeEveryWord(person?.name || 'Błąd')}
-							</p>
-							<p class="text-xs text-gray-600 dark:text-gray-400">
-								{person?.join_year || ''}
-							</p>
-						</div>
-					</div>
-				</td>
+				<PersonAvatarText picturesPromise={data.streamed.picturesList} {person} />
 				<ColorTag color={person.roles?.color} title={person.roles?.name} />
 				<td class="text-sm">{person.small_groups_name || 'Brak zastępu'}</td>
 				<td class="text-sm w-1/4">
--- src/routes/(private)/people/+page.svelte
+++ src/routes/(private)/people/+page.svelte
@@ -10,6 +10,7 @@
 	import { split } from 'postcss/lib/list';
 	import MainPage from '$lib/components/MainPage.svelte';
 	import TableView from '$lib/components/TableView.svelte';
+	import PersonAvatarText from '$lib/components/PersonAvatarText.svelte';
 	export let data: PageData;
 </script>
 
@@ -25,37 +26,7 @@
 				class="text-gray-700 dark:text-gray-400 hover:bg-gray-100 cursor-pointer dark:hover:bg-gray-700"
 				on:click={() => goto(`${$page.url}/person_details?id=${person.id}`)}
 			>
-				<td>
-					<div class="flex items-center text-sm">
-						<div class="relative hidden w-8 h-8 mr-3 rounded-full md:block">
-							{#await data.streamed.picturesList}
-								<img
-									class="object-cover w-full h-full rounded-full"
-									src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
-									alt=""
-									loading="lazy"
-								/>
-							{:then picturesList}
-								<img
-									class="object-cover w-full h-full rounded-full"
-									src={picturesList.find(
-										(picture) => person.id.toString() == picture.name.split('.')[0].toString()
-									)?.url?.signedUrl ||
-										'https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'}
-									alt=""
-									loading="lazy"
-								/>
-							{/await}
-							<div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true" />
-						</div>
-						<div>
-							<p class="font-semibold">{capitalizeEveryWord(person.name)}</p>
-							<p class="text-xs text-gray-600 dark:text-gray-400">
-								{person.join_year || ''}
-							</p>
-						</div>
-					</div>
-				</td>
+				<PersonAvatarText picturesPromise={data.streamed.picturesList} {person} />
 				<ColorTag color={person.roles?.color} title={person.roles?.name} />
 				<td class="text-sm">{person.small_groups?.name || 'Brak zastępu'}</td>
 				<ColorTag
