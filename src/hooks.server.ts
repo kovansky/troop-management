98a399ca799e1d3a93318b9df8fd824c10ebf74d
RMaster121 <rs.szczerba@hotmail.com>
2023-12-07 15:42:45 +0100

    Add conditional check for building environment

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,10 +4,16 @@ import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit';
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
 import { PRIVATE_SUPABASE_SERVICE_ROLE_KEY } from '$env/static/private';
+import { building } from '$app/environment';
 
 const privateRoutePrefix = '/(private)';
 
 export const handle: Handle = async ({ event, resolve }) => {
+	if (building) {
+		const response = await resolve(event);
+		return response;
+	}
+
 	if (event.route.id === null) {
 		throw error(404, 'What are you looking for?');
 	}
